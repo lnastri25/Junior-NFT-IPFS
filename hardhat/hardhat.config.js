3075d53255fca98c574c98729fc830d8091222a6
Lorenzo <lorenzonastri@gmail.com>
2023-01-04 15:48:54 -0300

    New ¡ NFT-IPFS

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+         version: "0.8.4",
+       },
+       {
+          version: "0.8.9",
+       },
+      ],
+  },
+  networks: {
+    mumbai: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
